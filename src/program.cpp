d1ae70fe08615a9fae176d5a27bbcc175e9a02d1
5253450 <5253450@c5058ba8-c010-11de-a759-a54c9d3330c2>
2009-11-19 07:33:24 +0000

    [changed] explosion generator

--- src/program.cpp
+++ src/program.cpp
@@ -875,44 +875,45 @@ void handleExplosions() {
 }
 
 void startExplosion() {
-	for (int i = 0; i < 20; i++) {
-		Particle* gruel = new Particle(player->TargetX + (rand() % 100) - 50,
-				player->TargetY + (rand() % 100) - 50, 128, 128, explTex[1]);
-		gruel->RMask = (float) (rand() % 50) / 100 + 0.5f;
-		gruel->GMask = (float) (rand() % 50) / 100;
-		gruel->BMask = (float) (rand() % 20) / 100;
-		gruel->AMask = (float) (rand() % 40) / 100 + 0.2f;
-		gruel->Scale = (float) (rand() % 80) / 100 + 0.2f;
-		gruel->XSpeed = (float) ((rand() % 100) - 100) / 1000;
-		gruel->YSpeed = (float) ((rand() % 100) - 100) / 1000;
-		gruel->TSpeed = (float) (rand() % 10 - 10) / 100;
-		gruel->RMod = -0.0001;
-		gruel->GMod = -0.0001;
-		gruel->BMod = -0.0001;
-		gruel->AMod = -0.0003;
-		gruel->ScaleMod = 0.0001;
-		explParticles.push_back(gruel);
-	}
-	for (int i = 0; i < 15; i++) {
-		Particle* spark = new Particle(player->TargetX + (rand() % 60) - 30,
-				player->TargetY + (rand() % 60) - 30, 128, 128, explTex[0]);
-		spark->RMask = spark->GMask = 1.0f;
-		spark->BMask = 0.6f;
-		spark->AMask = (float) (rand() % 50) / 100 + 0.5f;
-		spark->Scale = (float) (rand() % 40) / 100;
-		spark->XSpeed = (float) ((rand() % 130) - 130) / 1000;
-		spark->YSpeed = (float) ((rand() % 130) - 130) / 1000;
-		spark->AMod = -0.0006;
-		spark->ScaleMod = -0.0001;
-		explParticles.push_back(spark);
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 15; i++) {
+			Particle* gruel = new Particle(player->TargetX + (rand() % 100) - 50,
+					player->TargetY + (rand() % 100) - 50, 128, 128, explTex[1]);
+			gruel->RMask = (float) (rand() % 30) / 100 + 0.3f;
+			gruel->GMask = (float) (rand() % 30) / 100;
+			gruel->BMask = 0.0f;
+			gruel->AMask = (float) (rand() % 50) / 100 + 0.2f;
+			gruel->Scale = (float) (rand() % 80) / 100 + 0.2f;
+			gruel->XSpeed = (float) ((rand() % 100) - 50) / 1000;
+			gruel->YSpeed = (float) ((rand() % 100) - 50) / 1000;
+			gruel->TSpeed = (float) ((rand() % 20) - 10) / 100;
+			gruel->RMod = -0.0001;
+			gruel->GMod = -0.0001;
+			gruel->AMod = -0.0003;
+			gruel->ScaleMod = 0.0001;
+			explParticles.push_back(gruel);
+		}
+		for (int i = 0; i < 10; i++) {
+			Particle* spark = new Particle(player->TargetX + (rand() % 60) - 30,
+					player->TargetY + (rand() % 60) - 30, 128, 128, explTex[0]);
+			spark->RMask = spark->GMask = 1.0f;
+			spark->BMask = 0.6f;
+			spark->AMask = (float) (rand() % 40) / 100 + 0.6f;
+			spark->Scale = (float) (rand() % 40) / 100;
+			spark->XSpeed = (float) ((rand() % 250) - 125) / 1000;
+			spark->YSpeed = (float) ((rand() % 250) - 125) / 1000;
+			spark->AMod = -0.001;
+			spark->ScaleMod = -0.0002;
+			explParticles.push_back(spark);
+		}
 	}
 	Particle* baseSpark = new Particle(player->TargetX, player->TargetY, 128,
 			128, explTex[0]);
 	baseSpark->RMask = baseSpark->GMask = 1.0f;
-	baseSpark->BMask = 0.6f;
-	baseSpark->AMask = 0.3f;
-	baseSpark->AMod = -0.0004;
-	baseSpark->ScaleMod = -0.0001;
+	baseSpark->BMask = 0.8f;
+	baseSpark->AMask = 0.5f;
+	baseSpark->AMod = -0.0003;
 	explParticles.push_back(baseSpark);
 }
 
