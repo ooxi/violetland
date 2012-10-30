9cdb3f5756f4d0584c7e4de5c0dc1dc85db45ad0
ooxi <85fcd0ef4ec8@f977375cdcd6.anonbox.net>
2012-10-31 00:35:59 +0100

    Bumped to development Version `0.4.4~SNAPSHOT`
    Since the source code still reads Version `0.4.2` while `0.4.3` obviously is already released, the version had to be updated. As long as the source code carries the `~SNAPSHOT` suffix, the `master` branch reflects work in progress and not a stable release.

--- src/program.cpp
+++ src/program.cpp
@@ -70,7 +70,7 @@ using namespace std;
 using namespace violetland;
 
 const string PROJECT = "violetland";
-const string VERSION = "0.4.2";
+const string VERSION = "0.4.4~SNAPSHOT";
 const string DEFAULT_CHAR_NAME = "Violet";
 
 Configuration* config;
