e1c2463bff495b6267da6de8133f118188dd4301
ooxi <violetland@mail.ru>
2016-01-22 13:28:08 +0100

    DO NOT MERGE

---
+++ deploy/travis-ci/travis-ci.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+
+# @see http://stackoverflow.com/a/246128/2534648
+DIRECTORY_OF_TRAVIS_CI_SH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+
+#
+# Common environment
+# ==================
+#
+# BUILD_DATE		2016-01-22
+# TARGET		trusty-amd64-gcc
+# VERSION		0.5
+# TRAVIS_BUILD_NUMBER	167
+# TRAVIS_COMMIT		b6b90f7d2e8b7fdd4d1a62b59c7822ea160f9491
+#
+
+BUILD_DATE=`date --iso-8601`
+VERSION=`head -n 1 ${DIRECTORY_OF_TRAVIS_CI_SH}/../../VERSION`
+
+ARTIFACT_NAME_PREFIX="violetland-${VERSION}-b${TRAVIS_BUILD_NUMBER}-${TARGET}"
+
+
+
+#
+# Environment when building master
+# ================================
+# 
+# TRAVIS_BRANCH		master
+# TRAVIS_PULL_REQUEST	false
+#
+# ARTIFACT_NAME		violetland-0.5-b167-trusty-amd64-gcc
+#
+
+if [ "master" == "${TRAVIS_BRANCH}" ] && [ "false" == "${TRAVIS_PULL_REQUEST}" ]; then
+	ARTIFACT_NAME="${ARTIFACT_NAME_PREFIX}"
+fi
+
+
+#
+# Environment when building a different branch
+# =============================================
+#
+# TRAVIS_BRANCH		feature/automatic-windows-deployment
+# TRAVIS_PULL_REQUEST	false
+#
+# ARTIFACT_NAME		violetland-0.5-b167-trusty-amd64-gcc_feature-automatic-windows-deployment
+#
+
+if [ "master" != "${TRAVIS_BRANCH}" ] && [ "false" == "${TRAVIS_PULL_REQUEST}" ]; then
+
+	# @see http://serverfault.com/a/348485/207752
+	SAFE_BRANCH_NAME=$(echo "${TRAVIS_BRANCH}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
+	ARTIFACT_NAME="${ARTIFACT_NAME_PREFIX}_${SAFE_BRANCH_NAME}"
+fi
+
+
+#
+# Environment when building a pull request
+# ========================================
+#
+# TRAVIS_BRANCH		master
+# TRAVIS_PULL_REQUEST	104
+# 
+# ARTIFACT_NAME		violetland-0.5-b167-trusty-amd64-gcc_pull-request-104
+#
+
+if [ "false" != "${TRAVIS_PULL_REQUEST}" ]; then
+
+	# @see http://serverfault.com/a/348485/207752
+	SAFE_PULL_REQUEST=$(echo "${TRAVIS_PULL_REQUEST}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
+	ARTIFACT_NAME="${ARTIFACT_NAME_PREFIX}_${SAFE_PULL_REQUEST}"
+fi
+
--- deploy/travis-ci/trusty-amd64-gcc/script.sh
+++ deploy/travis-ci/trusty-amd64-gcc/script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "ABC"
-echo $TARGET
-echo $TRAVIS_BUILD_NUMBER
+
+
+# @see http://stackoverflow.com/a/246128/2534648
+DIRECTORY_OF_SCRIPT_SH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+
+
+
+source "${DIRECTORY_OF_SCRIPT_SH}/../travis-ci.sh"
+echo "X ${ARTIFACT_NAME} Y"
 
