2b1472ba89dbfb00aa5afdbcd9c7ca9ebfc85f48
ooxi <violetland@mail.ru>
2016-01-23 10:59:35 +0100

    DO NOT MERGE

--- deploy/travis-ci/windows-i686/after-success.sh
+++ deploy/travis-ci/windows-i686/after-success.sh
@@ -7,8 +7,5 @@ DIRECTORY_OF_THIS_FILE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIRECTORY_OF_THIS_FILE}/../travis-ci.sh"
 
 
-# Build
-CMAKE=cmake
-CXX=g++
-
-mkdir "${BUILD_DIRECTORY}" && cd "${BUILD_DIRECTORY}" && $CMAKE -DCMAKE_INSTALL_PREFIX=${DIST_DIRECTORY} "${ROOT_DIRECTORY}" && make install
+# Deploy
+deploy_as_zip
