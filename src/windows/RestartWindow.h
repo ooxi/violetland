d92ac1275d5ba312a56c6fd3acbb47bbafc1d5f5
Vasily Postnicov <shamaz.mazum@gmail.com>
2019-11-04 19:21:12 +0300

    Forgot to add gettext macro

--- src/windows/RestartWindow.h
+++ src/windows/RestartWindow.h
@@ -5,13 +5,15 @@
 
 #include "AlertWindow.h"
 
+#define _(STRING) gettext(STRING)
+
 namespace violet {
 	class RestartWindow : public AlertWindow {
 	private:
 		void onOKClick();
 	public:
 		RestartWindow(Configuration const* config, TextManager const* text) :
-			AlertWindow(config, text, "Restart the game to apply changes") {/* Do nothing */}
+			AlertWindow(config, text, _("Restart the game to apply changes")) {/* Do nothing */}
 		~RestartWindow() {/* Do noting */}
 	};
 }
